bad19e2d2af3213943ca23931e25c51ef4770526
Jan Šotek <honza.sotas@gmail.com>
2020-09-25 12:03:04 +0200

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
-const db = require('pg');
+const { Client } = require('pg');
 
 const db = knex({
     client: 'pg',
